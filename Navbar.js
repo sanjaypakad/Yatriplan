19f6b714e04a0b80ca9e739ed9bb09337dfa1b6a
sanjaypakad <77729934+sanjaypakad@users.noreply.github.com>
2024-11-16 22:50:27 +0530

    Update Navbar.js

--- Navbar.js
+++ Navbar.js
@@ -1,1 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+    return (
+        <nav>
+            <h1>Yatriplan</h1>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/destinations">Destinations</Link></li>
+                <li><Link to="/booking">Booking</Link></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
 
