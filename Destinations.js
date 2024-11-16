4b3292e7a1123e627be71b42dd3b0c88b29e0d40
sanjaypakad <77729934+sanjaypakad@users.noreply.github.com>
2024-11-16 22:56:13 +0530

    Update Destinations.js

--- Destinations.js
+++ Destinations.js
@@ -1,1 +1,13 @@
+import React from 'react';
 
+function Destinations() {
+    return (
+        <div>
+            <h2>Our Top Destinations</h2>
+            <p>Discover the most popular travel spots we offer.</p>
+            <img src="https://source.unsplash.com/featured/?destination,landscape" alt="Popular destination" />
+        </div>
+    );
+}
+
+export default Destinations;
