c74559c0a860dd8a3509f5f70bf6766f3732edb7
sanjaypakad <77729934+sanjaypakad@users.noreply.github.com>
2024-11-16 22:51:32 +0530

    Create Footer.js

---
+++ Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            <p>&copy; 2024 Yatriplan. All rights reserved.</p>
+            <img src="https://source.unsplash.com/featured/?travel,footer" alt="Footer travel" />
+        </footer>
+    );
+}
+
+export default Footer;
